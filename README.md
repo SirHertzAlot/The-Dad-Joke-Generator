5bce1f075c038010f561b6ac6adf0ed2a771f89f
SirHertzAlot <dukjoseph@gmail.com>
2022-07-01 01:43:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-jrs8p9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-jrs8p9)
---
+++ index.js
@@ -0,0 +1,18 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const joke = document.querySelector('#joke');
+const refreshBtn = document.querySelector('#refreshBtn');
+const uri = 'https://icanhazdadjoke.com/';
+
+let xhr = new XMLHttpRequest();
+xhr.addEventListener('load', function () {
+  joke.innerHTML = `<h1>${this.responseText}</h1>`;
+});
+
+refreshBtn.addEventListener('click', function () {
+  xhr.open('GET', uri);
+  xhr.setRequestHeader('Accept', 'text/plain');
+  xhr.send();
+});
